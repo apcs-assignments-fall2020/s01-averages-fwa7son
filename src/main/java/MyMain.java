948d3b00b4f9f0e6ef436e14ab91a01a4045cede
fwa7son <21fwatson@athenian.org>
2021-01-19 00:00:57 +0000

    functions passed tests in main class but not in make test.

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,25 +2,96 @@ public class MyMain {
 
     // Returns the mean of the 2D array mat
     public static double mean(double[][] mat) { 
-        // YOUR CODE HERE
-        return -1.0;
+        // mean is total divded by the number of elements in a list
+        double arrTotal = 0;
+        int arrLength = 0;
+        for(int i = 0; i < mat.length; i++)
+        {
+            for(int g = 0; g<mat[i].length; g++)
+            {
+                arrTotal = arrTotal + mat[i][g];
+                arrLength = arrLength + 1;
+            }
+        }
+        double ResultMean = arrTotal/arrLength;
+        return ResultMean;
     }
 
     // Returns the median of the 2D array mat
     public static double median(double[][] mat) { 
-        // YOUR CODE HERE
-        return -1.0;
+        // median is the middle number
+        int size = mat.length * mat[0].length;
+        double[] oneArray = new double[size];
+
+        int position = 0;
+
+        double ResultMedian = 0;
+        for(int i = 0; i < mat.length; i++)
+        {
+            for(int g = 0; g<mat[i].length; g++)
+            {
+                oneArray[position] = mat[i][g];
+                position++;
+            }
+        }
+        if(size % 2 != 0)
+        {
+            ResultMedian = oneArray[size/2];
+        }
+        else
+        {
+            //average of 2 middle numbers
+            //ResultMedian = (oneArray[size])
+            ResultMedian = (((oneArray[size/2] + oneArray[size/2])-1)/2);
+        }
+        return ResultMedian;
     }
     
 
     // Returns the mode of the 2D array mat
     public static double mode(double[][] mat) { 
-        // YOUR CODE HERE
-        return -1.0;
+        // mode is the most frequennt number
+        // 2d --> 1d then find mode of new 1d array
+        double ResultMode = 0;
+        int size = mat.length * mat[0].length;
+        double[] oneArray = new double[size];
+            
+        int position1 = 0;
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) { 
+                oneArray[position1] = (mat[i][j]); 
+                position1 ++;
+            }
+        }
+        for(int i = 0; i < size; i++){
+            int position2 = 0;
+            for(int j = 0; j < size; j++)
+            {
+                if (oneArray[i]==oneArray[j])
+                {
+                    position2++;
+                    if(position2 > ResultMode)
+                    {
+                        ResultMode = position2-1;
+                        ResultMode = oneArray[i];
+                    }
+                }
+            }
+        }
+
+        return ResultMode;
     }
 
 
     public static void main(String[] args) {
-        // Write some code here to test your methods!
+        double[][] m1 = {
+            {2,  2,  4,  6},
+            {7,  9,  10, 12},
+            {14, 15, 17, 18},
+            {22, 23, 27, 30}
+        };
+        System.out.println("Mean is " + mean(m1));
+        System.out.println("Median is " + median(m1));
+        System.out.println("Mode is " + mode(m1));
     }
 }
